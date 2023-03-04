6ea89b86116da8cf8c74bc28b015374beb718575
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-03-04 14:41:45 +0700

    add inverse permute

--- PBox.py
+++ PBox.py
@@ -7,3 +7,10 @@ class PBox():
         for i in range(len(input)):
             array.append(input[self.pbox_table[i]])
         return array
+
+    def inverse_permute(self, input):
+        array =[]
+        for i in range(len(input)):
+            idx = self.pbox_table.index(i)
+            array.append(input[idx])
+        return array
